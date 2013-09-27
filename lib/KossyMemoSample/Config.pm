a8ed9fbd463019b5a8ff6b83a02c7f4c1124c7d7
Sho Nakatani <lay.sakura@gmail.com>
2013-09-27 09:49:32 +0900

    adds Config.pm Signed-off-by: Sho Nakatani <lay.sakura@gmail.com>

---
+++ lib/KossyMemoSample/Config.pm
@@ -0,0 +1,21 @@
+package KossyMemoSample::Config;
+
+use Config::ENV 'PLACK_ENV', export => 'config';
+
+config development => +{
+    db => $ENV{DB_DB} || "test",
+    host => $ENV{DB_HOST} || "localhost",  # 0.0.0.0:3306
+    user => $ENV{DB_USER} || "root",
+    pass => $ENV{DB_PASS} || "",
+    table => $ENV{DB_TABLE} || "memo",
+};
+
+config production => +{
+    db => $ENV{DB_DB} || "test",
+    host => $ENV{DB_HOST} || "localhost",  # 0.0.0.0:3306
+    user => $ENV{DB_USER} || "root",
+    pass => $ENV{DB_PASS} || "",
+    table => $ENV{DB_TABLE} || "memo",
+};
+
+1;
